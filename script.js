7624562f0984e06d892e90df7fb6e5e0b6c33ae3
Dahir Abdi <dahirabdi@Dahirs-MacBook-Pro.local>
2021-08-27 10:53:28 -0400

    the colors change are done

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var date = moment().format("dddd, MMM Do");
 $("#currentDay").text(date);
-    
+var hour = moment().format("HH");
+var timeblocks = $(".time-block")
+//console.log(hour)
+console.log(timeblock);
+for (let i = 0; i < timeblocks.length; i++) {
+    var timeblock = timeblocks[i];
+    console.log(timeblock.id)
+    if (parseInt(timeblock.id) < hour) {
+        timeblock.setAttribute("class", "past");
+    }
+    else if (parseInt(timeblock.id) > hour) {
+        timeblock.setAttribute("class", "future");
+    }
+    else
+    timeblock.setAttribute("class", "present");
+}
+
+function name(params) {
+    $()
+}
